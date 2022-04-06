d61f17b91a27f9b0b6aceee95a1bc1d5daee01c7
Alexander Rovang <DXRovang@gmail.com>
2022-04-06 10:35:38 -0400

    adds usersController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  get '/signup', to: "users#new" # note: '/signup' = signup_path
+  post '/users', to: "users#create"
+  
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users, except: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
