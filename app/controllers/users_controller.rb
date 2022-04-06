1f657a07fc1ec07f372ed3408f9b292796b475e9
Alexander Rovang <DXRovang@gmail.com>
2022-04-06 10:44:09 -0400

    adds serializers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,20 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    render json: @users
   end
 
   def new
     #renamed signup
     @user = User.new
+    render json: @user
   end 
 
   def create 
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      render json: @user
     else
       render :new
     end
@@ -26,13 +28,13 @@ class UsersController < ApplicationController
   def edit
     #probably could achieve this with if !current_user
     if params[:id].to_i != session[:user_id]
-      redirect_to user_path(@user)
+      render json: @user
     end
   end
 
   def update
     if @user.update(user_params)
-      redirect_to user_path(@user)
+      render json: @user
     else
       render :new
     end
